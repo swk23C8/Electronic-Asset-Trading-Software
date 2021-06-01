a3ebe7d625aac88a385e5df4847a20ee5f6d64c6
DanieNG <n10653155@qut.edu.au>
2021-06-01 17:14:25 +1000

    Refactoring

--- src/Server/ServerManagement.java
+++ src/Server/ServerManagement.java
@@ -1,5 +1,6 @@
 package Server;
 
+import Common.CommandMethods;
 import Common.SocketMessages;
 import Common.Offer;
 
@@ -7,6 +8,7 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
+import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -131,6 +133,64 @@ public class ServerManagement {
             }
         }
     }
+
+    /**
+     * Command handling for server
+     */
+
+
+
+
+    public void Reconcile()
+    {
+        OfferDataSource o = new OfferDataSource();
+        List<Offer> list = o.offerSet();
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < list.size(); j++){
+                if (i == j) {
+                    continue;
+                }
+                Integer id1 = list.get(i).getId();
+                Integer id2 = list.get(j).getId();
+                String type1 = list.get(i).getOfferType();
+                String type2 = list.get(j).getOfferType();
+                String ou1 = list.get(i).getOUName();
+                String ou2 = list.get(j).getOUName();
+                String asset1 = list.get(i).getAssetName();
+                String asset2 = list.get(j).getAssetName();
+                Integer qty1 = list.get(i).getQuantity();
+                Integer qty2 = list.get(j).getQuantity();
+                Integer price1 = list.get(i).getCreditsEach();
+                Integer price2 = list.get(j).getCreditsEach();
+
+                if ((i < j) && (!type1.equals(type2))
+                        && (!ou1.equals(ou2))
+                        && (asset1.equals(asset2))
+                        && (qty1.equals(qty2)) && (price1.equals(price2))) {
+                    o.deleteOffer(id1);
+                    o.deleteOffer(id2);
+                    Offer tmp1 = list.get(i);
+                    Offer tmp2 = list.get(j);
+                    list.remove(i);
+                    list.add(0, tmp1);
+                    list.remove(j);
+                    list.add(1, tmp2);
+                    break;
+                }
+            }
+        }
+
+        System.out.println(list.get(7).getOfferType());
+
+//        Hashset<OU> ous = ...;
+
+//        OU has creditbalance, quantity of each asset/ type of asset as ke;
+//        HashMap<AssetName, Int> ouassets = ;
+//        int credit = 0;
+//        Hashset<Offer> alltrades;
+//        Offer
+    }
+
 
     /**
      * Requests the server to shut down

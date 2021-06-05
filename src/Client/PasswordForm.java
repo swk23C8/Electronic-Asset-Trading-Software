f259d425c32599553008fc040d5a2994f767e010
swk23C8 <35516289+swk23C8@users.noreply.github.com>
2021-06-06 01:07:23 +0900

    Added "Confirm Change" button to change password gui, placeholder label for all values and event listeners.

--- src/Client/PasswordForm.java
+++ src/Client/PasswordForm.java
@@ -1,9 +1,21 @@
 package Client;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class PasswordForm {
     private JPasswordField passwordField2;
     private JPasswordField passwordField3;
     private JPasswordField passwordField1;
+    private JButton confirmUpdateButton;
+
+    public PasswordForm() {
+        confirmUpdateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }

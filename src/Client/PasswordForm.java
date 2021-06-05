97671b3952d2f3ba699813ca688f276b783959b1
swk23C8 <35516289+swk23C8@users.noreply.github.com>
2021-06-06 01:02:00 +0900

    Adding settings (change password gui)

--- src/Client/PasswordForm.java
+++ src/Client/PasswordForm.java
@@ -1,4 +1,9 @@
 package Client;
 
+import javax.swing.*;
+
 public class PasswordForm {
+    private JPasswordField passwordField2;
+    private JPasswordField passwordField3;
+    private JPasswordField passwordField1;
 }

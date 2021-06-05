9e9329867f41629ca6880589e07031f008128368
swk23C8 <35516289+swk23C8@users.noreply.github.com>
2021-06-05 17:31:43 +0900

    Done admin form

--- src/Client/AdminForm.java
+++ src/Client/AdminForm.java
@@ -8,4 +8,18 @@ public class AdminForm {
     private JButton button1;
     private JButton button2;
     private JList list1;
+    private JTextField textField2;
+    private JPasswordField passwordField1;
+    private JTextField textField3;
+    private JPasswordField passwordField2;
+    private JPasswordField passwordField3;
+    private JTextField textField4;
+    private JList list2;
+    private JButton button3;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JButton button4;
+    private JButton button5;
+    private JPasswordField passwordField4;
+    private JButton button6;
 }

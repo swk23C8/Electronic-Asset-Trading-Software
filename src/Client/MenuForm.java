ebd8e5c0016b7239410340da3b8616f5ded034f4
swk23C8 <35516289+swk23C8@users.noreply.github.com>
2021-06-06 19:59:20 +0900

    finishing connecting all the gui screens so users can navigate through each gui with a button press

--- src/Client/MenuForm.java
+++ src/Client/MenuForm.java
@@ -1,5 +1,7 @@
 package Client;
 
+import org.junit.jupiter.api.Order;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -17,31 +19,66 @@ public class MenuForm extends JFrame{
 //        WHATEVERONLYADMINSEES.setVisible(yesno);
     }
 
+    private void organisationButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        OrganisationForm organisationForm = new OrganisationForm();
+        organisationForm.setContentPane(new OrganisationForm().organisationPanel);
+        organisationForm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        organisationForm.setVisible(true);
+        organisationForm.pack();
+        organisationForm.setTitle("CAB302");
+    }
+
+    private void allOrdersButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        OrderForm orderForm = new OrderForm();
+        orderForm.setContentPane(new OrderForm().orderPanel);
+        orderForm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        orderForm.setVisible(true);
+        orderForm.pack();
+        orderForm.setTitle("CAB302");
+    }
+
+    private void adminMenuButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        AdminForm adminForm = new AdminForm();
+        adminForm.setContentPane(new AdminForm().adminPanel);
+        adminForm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        adminForm.setVisible(true);
+        adminForm.pack();
+        adminForm.setTitle("CAB302");
+    }
+
+    private void changePasswordButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        PasswordForm passwordForm = new PasswordForm();
+        passwordForm.setContentPane(new PasswordForm().passwordPanel);
+        passwordForm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        passwordForm.setVisible(true);
+        passwordForm.pack();
+        passwordForm.setTitle("CAB302");
+    }
     public MenuForm() {
         organisationButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                organisationButtonActionPerformed(e);
             }
         });
 
         allOrdersButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                allOrdersButtonActionPerformed(e);
             }
         });
 
         adminMenuButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                adminMenuButtonActionPerformed(e);
             }
         });
         changePasswordButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                changePasswordButtonActionPerformed(e);
             }
         });
     }
--- src/Client/OrderForm.java
+++ src/Client/OrderForm.java
@@ -4,12 +4,13 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class OrderForm {
+public class OrderForm extends JFrame{
     private JButton BUYButton;
     private JButton SELLButton;
     private JTextField textField1;
     private JTextField textField2;
     private JList list1;
+    public JPanel orderPanel;
 
     public OrderForm() {
         BUYButton.addActionListener(new ActionListener() {

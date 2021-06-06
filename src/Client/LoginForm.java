f3a4c0aa2a998f6e378466acc9f3b704bef71875
swk23C8 <35516289+swk23C8@users.noreply.github.com>
2021-06-06 15:38:50 +0900

    merge conflict on loginform

--- src/Client/LoginForm.java
+++ src/Client/LoginForm.java
@@ -1,7 +1,6 @@
 package Client;
 
 import Server.DBConnection;
-import Server.UserDataSource;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -11,49 +10,84 @@ import java.sql.ResultSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class LoginForm extends JFrame {
+public class LoginForm extends JFrame{
     private JTextField usernameField;
     private JPasswordField passwordField;
     private JButton loginButton;
     private JPanel loginPanel;
 
+    // maybe this?
+    private void loginButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        String username = usernameField.getText();
+        String password = String.valueOf(passwordField.getPassword());
 
-    private void loginButtonActionPerformed(ActionEvent e) {
-        {
-            String username = usernameField.getText();
-            String password = String.valueOf(passwordField.getPassword());
+        if(username.trim().equals("") && password.trim().equals("")){
+            JOptionPane.showMessageDialog(rootPane, "Please enter both username and password");
+        }
+        else if(username.trim().equals("")){
+            JOptionPane.showMessageDialog(rootPane, "Please enter username");
+        }
+        else if (password.trim().equals("")){
+            JOptionPane.showMessageDialog(rootPane, "Please enter password");
+        }
+        else{
+            String loginQuery = "SELECT * FROM user WHERE `username` = ? and `password` = ? ";
+            try {
 
-            if (username.trim().equals("") && password.trim().equals("")) {
-                JOptionPane.showMessageDialog(rootPane, "Please enter both username and password");
-            } else if (username.trim().equals("")) {
-                JOptionPane.showMessageDialog(rootPane, "Please enter username");
-            } else if (password.trim().equals("")) {
-                JOptionPane.showMessageDialog(rootPane, "Please enter password");
-            } else {
-                String loginQuery = "SELECT * FROM user WHERE `username` = ? and `password` = ? ";
-                try {
-
-                    UserDataSource userDatabase = new UserDataSource();
-
-                    if (userDatabase.getUser(username).getPassword().
-                            equals(userDatabase.passwordCheck(password, userDatabase.getUser(username)))) {
-                        //I cant seem to hide/close the window...
-                        setVisible(false);
-                        dispose();
-                        MenuForm menuForm = new MenuForm();
-                        menuForm.setContentPane(new MenuForm().menuPanel);
-                        menuForm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                        menuForm.setVisible(true);
-                        menuForm.pack();
-                        menuForm.setTitle("CAB302");
-                        menuForm.enableuser(true);
-                    } else
-                        JOptionPane.showMessageDialog(loginButton, "Wrong Username or Password");
-                } catch (Exception ex) {
-                    JOptionPane.showMessageDialog(loginButton, "Wrong Username or Password");
-                    Logger.getLogger(LoginForm_1.class.getName()).log(Level.SEVERE, null, ex);
+                PreparedStatement ps = DBConnection.getInstance().prepareStatement(loginQuery);
+                PreparedStatement ps1 = DBConnection.getInstance().prepareStatement(loginQuery);
+                ps.setString(1, username);
+                ps.setString(2, password);
+                ps1.setString(1, username);
+                ps1.setString(2, password);
+                ResultSet rs = ps.executeQuery();
+                ResultSet rs1 = ps1.executeQuery();
+                if(rs.next()){
+                    //I cant seem to hide/close the window...
+                    this.dispose();
+                    MenuForm menuForm = new MenuForm();
+                    menuForm.setContentPane(new MenuForm().menuPanel);
+                    menuForm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    menuForm.setVisible(true);
+                    menuForm.pack();
+                    menuForm.setTitle("CAB302");
                 }
+                else if(rs1.next()){
+                    //I cant seem to hide/close the window...
+                    this.dispose();
+                    MenuForm menuForm = new MenuForm();
+                    menuForm.setContentPane(new MenuForm().menuPanel);
+                    menuForm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    menuForm.setVisible(true);
+                    menuForm.pack();
+                    menuForm.setTitle("CAB302");
+                    menuForm.enableuser(true);
+                }
+                else
+                    JOptionPane.showMessageDialog(loginButton, "Wrong Username or Password");
+            }
+            catch (Exception ex) {
+                Logger.getLogger(LoginForm_1.class.getName()).log(Level.SEVERE,null,ex);
             }
         }
+    }
+
+
+    public static void main(String[] args) {
+        LoginForm loginForm = new LoginForm();
+        loginForm.setContentPane(new LoginForm().loginPanel);
+        loginForm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        loginForm.setVisible(true);
+        loginForm.pack();
+        loginForm.setTitle("CAB302");
+    }
+
+    public LoginForm() {
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                loginButtonActionPerformed(e);
+            }
+        });
     }
 }

8235844c9f3f790e155ff188657ec61f8ab1d341
swk23C8 <35516289+swk23C8@users.noreply.github.com>
2021-06-06 15:32:42 +0900

    took out the this.dispose(); isnt showing any errors but its not working.

--- src/Client/LoginForm.java
+++ src/Client/LoginForm.java
@@ -11,55 +11,49 @@ import java.sql.ResultSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class LoginForm extends JFrame{
+public class LoginForm extends JFrame {
     private JTextField usernameField;
     private JPasswordField passwordField;
     private JButton loginButton;
     private JPanel loginPanel;
 
 
-    private void loginButtonActionPerformed(ActionEvent e){
+    private void loginButtonActionPerformed(ActionEvent e) {
         {
             String username = usernameField.getText();
             String password = String.valueOf(passwordField.getPassword());
 
-            if(username.trim().equals("") && password.trim().equals("")){
+            if (username.trim().equals("") && password.trim().equals("")) {
                 JOptionPane.showMessageDialog(rootPane, "Please enter both username and password");
-            }
-            else if(username.trim().equals("")){
+            } else if (username.trim().equals("")) {
                 JOptionPane.showMessageDialog(rootPane, "Please enter username");
-            }
-            else if (password.trim().equals("")){
+            } else if (password.trim().equals("")) {
                 JOptionPane.showMessageDialog(rootPane, "Please enter password");
-            }
-            else{
+            } else {
                 String loginQuery = "SELECT * FROM user WHERE `username` = ? and `password` = ? ";
                 try {
 
-                        UserDataSource userDatabase = new UserDataSource();
+                    UserDataSource userDatabase = new UserDataSource();
 
-                        if(userDatabase.getUser(username).getPassword().
-                                equals(userDatabase.passwordCheck(password, userDatabase.getUser(username)))){
-                            //I cant seem to hide/close the window...
-                            setVisible(false);
-                            dispose();
-                            MenuForm menuForm = new MenuForm();
-                            menuForm.setContentPane(new MenuForm().menuPanel);
-                            menuForm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                            menuForm.setVisible(true);
-                            menuForm.pack();
-                            menuForm.setTitle("CAB302");
-                            menuForm.enableuser(true);
-                        }
-                        else
-                            JOptionPane.showMessageDialog(loginButton, "Wrong Username or Password");
-                    }
-                    catch (Exception ex) {
+                    if (userDatabase.getUser(username).getPassword().
+                            equals(userDatabase.passwordCheck(password, userDatabase.getUser(username)))) {
+                        //I cant seem to hide/close the window...
+                        setVisible(false);
+                        dispose();
+                        MenuForm menuForm = new MenuForm();
+                        menuForm.setContentPane(new MenuForm().menuPanel);
+                        menuForm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                        menuForm.setVisible(true);
+                        menuForm.pack();
+                        menuForm.setTitle("CAB302");
+                        menuForm.enableuser(true);
+                    } else
                         JOptionPane.showMessageDialog(loginButton, "Wrong Username or Password");
-                        Logger.getLogger(LoginForm_1.class.getName()).log(Level.SEVERE,null,ex);
-                    }
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(loginButton, "Wrong Username or Password");
+                    Logger.getLogger(LoginForm_1.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
-        });
+        }
     }
 }
